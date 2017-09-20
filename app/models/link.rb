e46d16e6007c117e7de8fa97840e6c18d16e279d
Marcus Rands <mrands@hotmail.co.uk>
2017-09-20 16:53:24 +0100

    Third feature test now passing. Functionality to add tags added

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,18 +1,15 @@
 require 'data_mapper'
 require 'dm-postgres-adapter'
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+
 class Link
 
   include DataMapper::Resource
+
+  has n, :tags, through: Resource
 
   property :id, Serial
   property :title, String
   property :url, String
 
 end
-
-
-DataMapper.finalize
-
-DataMapper.auto_upgrade!
